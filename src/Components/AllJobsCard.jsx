03b427e5ae79dc2f9bef41eebce7db4bf137a969
maharib820 <maharib820@gmail.com>
2023-11-07 21:05:02 +0600

    data successfully receive using category

--- src/Components/AllJobsCard.jsx
+++ src/Components/AllJobsCard.jsx
@@ -1,4 +1,19 @@
-const AllJobsCard = ({category}) => {
+import { useEffect, useState } from "react";
+
+const AllJobsCard = ({ category }) => {
+
+    const [allJobs, setAllJobs] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/allAddedJobs/${category.category}`)
+            .then(res => res.json())
+            .then(data => {
+                setAllJobs(data)
+            })
+    }, [category.category])
+
+    console.log(allJobs);
+
     return (
         <div>
             <h2>{category.category}</h2>
