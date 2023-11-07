b862f41df616e8b4b596f949420cb4e7c306d2e0
maharib820 <maharib820@gmail.com>
2023-11-08 01:12:48 +0600

    in added job page data receive user wise

--- src/Pages/AddedJobs/AddedJobs.jsx
+++ src/Pages/AddedJobs/AddedJobs.jsx
@@ -1,7 +1,22 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+
 const AddedJobs = () => {
+
+    const { user } = useContext(AuthContext);
+    const [myAddedJobs, setMyAddedJobs] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/allAddedJobs?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                setMyAddedJobs(data)
+            })
+    }, [user?.email])
+
     return (
         <div>
-            
+
         </div>
     );
 };
